3e850060c6892af7ef339f0040c409c73c2b4ad4
MomoFlora <2519840456@qq.com>
2026-03-06 15:07:53 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -66,12 +66,12 @@ git clone https://github.com/Xiaokailnol/packages_utils_runc feeds/packages/util
 
 # Realtek Ethernet driver - R8168 & R8125 & R8126 & R8152 & R8101 & r8127
 rm -rf package/kernel/{r8168,r8101,r8125,r8126,r8127}
-git clone https://$github/sbwml/package_kernel_r8168 package/kernel/r8168
-git clone https://$github/sbwml/package_kernel_r8152 package/kernel/r8152
-git clone https://$github/sbwml/package_kernel_r8101 package/kernel/r8101
-git clone https://$github/sbwml/package_kernel_r8125 package/kernel/r8125
-git clone https://$github/sbwml/package_kernel_r8126 package/kernel/r8126
-git clone https://$github/sbwml/package_kernel_r8127 package/kernel/r8127
+git clone https://github.com/sbwml/package_kernel_r8168 package/kernel/r8168
+git clone https://github.com/sbwml/package_kernel_r8152 package/kernel/r8152
+git clone https://github.com/sbwml/package_kernel_r8101 package/kernel/r8101
+git clone https://github.com/sbwml/package_kernel_r8125 package/kernel/r8125
+git clone https://github.com/sbwml/package_kernel_r8126 package/kernel/r8126
+git clone https://github.com/sbwml/package_kernel_r8127 package/kernel/r8127
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
